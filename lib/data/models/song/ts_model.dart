aa2c5a416dc1d81ab176c85a89400d8641107c25
Artemy Bobrov <a.bobrov@galament.com>
2025-08-11 15:31:30 +0500

    add audio caching

---
+++ lib/data/models/song/ts_model.dart
@@ -0,0 +1,13 @@
+class CachedSong {
+  CachedSong({
+    required this.keyURL,
+    required this.fileURL,
+    required this.duration,
+    required this.name,
+  });
+
+  final String keyURL;
+  final String fileURL;
+  final String duration;
+  final String name;
+}
